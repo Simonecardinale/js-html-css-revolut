4ee9cdb911d03c9c75e714f5e0a89e1214d1fe09
Simone Cardinale <simonecardinale8@gmail.com>
2021-01-12 16:50:19 +0100

    dropmenu bug fixed

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-    $('.container_right .menu_right li').hover(function(){
+    $('.container_right .menu_right li:not(.menu_right li:last-child)').hover(function(){
         $(this).children().toggleClass('display_block');
     });
     $('.box').hover(function(){
