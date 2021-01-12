66f31ebe4e72626cdeaab86abfb62110cc451a0f
Simone Cardinale <simonecardinale8@gmail.com>
2021-01-12 16:55:41 +0100

    dropmenu bug fixed + hover on download CTA

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-    $('.container_right .menu_right li:not(.menu_right li:last-child)').hover(function(){
+    $('.container_right .menu_right li:not(.download_cta)').hover(function(){
         $(this).children().toggleClass('display_block');
     });
     $('.box').hover(function(){
